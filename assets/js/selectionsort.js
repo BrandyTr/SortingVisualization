81aae5487b76fd0553643837738dd8754bece94e
Brandy Tran <baotutran1103@gmail.com>
2024-06-16 01:44:29 +0700

    fix bug selection

--- assets/js/selectionsort.js
+++ assets/js/selectionsort.js
@@ -7,9 +7,12 @@ class SelectionSort extends SortStrategy {
         updatePauseBtn();
         
         // Selection Sort Algorithm
+        var minIndex = 0;
+
         for (var i = 0; i < blocks.length; i++) {
-            var minIndex = i;
-            blocks[minIndex].style.backgroundColor = "#B2533E";
+            minIndex = i;
+
+            blocks[i].style.backgroundColor = "#B7B597";
         
             for (var j = i + 1; j < blocks.length; j++) {
                 
@@ -32,26 +35,50 @@ class SelectionSort extends SortStrategy {
                 var value2 = Number(blocks[j].childNodes[0].innerHTML);
         
                 if (value1 > value2) {
-                    if (minIndex !== i) blocks[minIndex].style.backgroundColor = "#3F9BBF";
+                    if (minIndex !== i) {
+                        blocks[minIndex].style.backgroundColor = "#3F9BBF";
+                    }
                     minIndex = j;
-                    blocks[minIndex].style.backgroundColor = "#B2533E";
                 } else {
                     blocks[j].style.backgroundColor = "#3F9BBF";
                 }
             }
         
             if (minIndex !== i) {
-                await swap(blocks[i], blocks[minIndex]);
+                await this.swap(blocks[i], blocks[minIndex]);
+                // await swap(blocks[i], blocks[minIndex]);
                 blocks = document.querySelectorAll(".block");
             }
         
+            blocks[minIndex].style.backgroundColor = "#3F9BBF";
             blocks[i].style.backgroundColor = "#7A9D54";
         }
         
         isSorting = false;
         currentSort = null;
         updatePauseBtn();
+    }
 
+     // Swap function
+    async swap(ele1, ele2) {
+        return new Promise((resolve) => {
+            // Swap the style heights
+            var tempHeight = ele1.style.height;
+            ele1.style.height = ele2.style.height;
+            ele2.style.height = tempHeight;
+
+            // Swap the labels
+            var tempLabel = ele1.childNodes[0].innerHTML;
+            ele1.childNodes[0].innerHTML = ele2.childNodes[0].innerHTML;
+            ele2.childNodes[0].innerHTML = tempLabel;
+
+            // Wait for the transition to end!
+            window.requestAnimationFrame(function() {
+                setTimeout(() => {
+                    resolve();
+                }, 250);
+            });
+        });
     }
 }
 
